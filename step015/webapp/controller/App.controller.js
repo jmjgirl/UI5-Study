12f87d2d9d82fa356934eea9227aa2589c8b0bcb
jmjgirl <jmjcap123@gmail.com>
2025-10-21 09:26:12 +0900

    Nested Views

---
+++ step015/webapp/controller/App.controller.js
@@ -0,0 +1,16 @@
+sap.ui.define([
+   "sap/ui/core/mvc/Controller"
+], function (Controller) {
+   "use strict";
+
+   return Controller.extend("step015.controller.App", {
+     // onShowHello: function(){
+     //   // read msg from i18n model
+     //   var oBundle = this.getView().getModel("i18n").getResourceBundle();
+     //   var sRecipient = this.getView().getModel().getProperty("/recipient/name");
+     //   var sMsg = oBundle.getText("helloMsg", [sRecipient]);
+     //   // show message
+     //   MessageToast.show(sMsg);
+     // }
+   });
+});
