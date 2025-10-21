d92005c84278627b89d6f690e22c7e27e734c2e7
jmjgirl <jmjcap123@gmail.com>
2025-10-21 11:10:05 +0900

    ing

---
+++ step019/webapp/controller/App.controller.js
@@ -0,0 +1,11 @@
+sap.ui.define([
+   "sap/ui/core/mvc/Controller"
+], function (Controller) {
+   "use strict";
+
+   return Controller.extend("step019.controller.App", {
+     onOpenDialog : function (){
+ 			this.getOwnerComponent().openHelloDialog();
+ 		}
+   });
+});
