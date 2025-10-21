ff22580afd24e1450b77f41693acd90a0a4c9741
jmjgirl <jmjcap123@gmail.com>
2025-10-21 15:22:41 +0900

    Expression Bindings

---
+++ step022/webapp/controller/HelloDialog.js
@@ -0,0 +1,42 @@
+// Dialog를 Open하고 Exit하는 기능을 따로 관리
+sap.ui.define([
+	"sap/ui/base/ManagedObject",
+	"sap/ui/core/Fragment"
+], function (ManagedObject, Fragment) {
+	"use strict";
+
+	return ManagedObject.extend("step022.controller.HelloDialog", {
+		constructor : function(oView){
+			this._oView = oView;
+		},
+
+		exit : function (){
+			delete this._oView;
+		},
+
+		open : function (){
+			var oView = this._oView;
+
+			// create dialog lazily
+			if (!oView.byId("helloDialog")) {
+				var oFragmentController = {
+					onCloseDialog : function () {
+						oView.byId("helloDialog").close();
+					}
+				};
+				// load asynchronous XML fragment
+				Fragment.load({
+					id: oView.getId(),
+					name: "step022.view.HelloDialog",
+					controller: oFragmentController
+				}).then(function (oDialog) {
+					// connect dialog to the root view of this component (models, lifecycle)
+					oView.addDependent(oDialog);
+					oDialog.open();
+				});
+			} else {
+				oView.byId("helloDialog").open();
+			}
+		}
+	});
+});
