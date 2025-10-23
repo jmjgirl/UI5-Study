bdd642b21f18db15627aeb98b40de733106db801
jmjgirl <jmjcap123@gmail.com>
2025-10-23 14:57:55 +0900

    UI5 기초 실습

--- zstep099/webapp/controller/App.controller.js
+++ zstep099/webapp/controller/App.controller.js
@@ -12,8 +12,26 @@ sap.ui.define([
 
         var oStateModel = models.createState();
         this.getView().setModel(oStateModel, "stateModel");
+
+        var oButton = new sap.m.Button('newBtn', { text : "New Button", press: this.newBtnClick.bind(this) });
+        var oPage = this.getView().byId("myPage");
+        oPage.addContent(oButton);
+
+        console.log("onInit");
     },
-    
+
+    onBeforeRendering: function(){
+      console.log("onBeforeRendering");
+    },
+
+    onAfterRendering: function(){
+      console.log("onAfterRendering");
+    },
+
+    onExit: function(){
+      console.log("onExit");
+    },
+
     changeState(){
       var oModel = this.getView().getModel("stateModel");
       var bState = oModel.getProperty("/enabled");
@@ -22,6 +40,11 @@ sap.ui.define([
       }else{
         oModel.setProperty("/enabled",true);
       }
+    },
+
+    newBtnClick: function(){
+      alert("New Button is Clicked");
+      this.getView().byId("btn01").setProperty("text", "New Name");  //실행이 안됨
     }
   });
 });
